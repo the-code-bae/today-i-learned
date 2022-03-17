323a475ee6cf0a29c0abc164d4d92242f008d190
Maka I <nibe0993@gmail.com>
2022-03-17 15:31:35 +0000

    Add new file

---
+++ appscript/daily_agenda_forwarder.js
@@ -0,0 +1,20 @@
+//https://medium.com/@costaparas/time-based-forwarding-of-emails-with-google-apps-script-aff0b59a2f7b
+
+//Daily Agenda Forwarder
+
+function main() {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  let i = 2;
+  while (true) {
+    const searchParameters = sheet.getRange(i, 1).getValue();
+    if (!searchParameters) {
+      break;
+    }
+    const threads = GmailApp.search(searchParameters);
+    if (threads.length) {
+      const emails = sheet.getRange(i, 2).getValue();
+      threads[0].getMessages()[0].forward(emails);
+    }
+    i++;
+  }
+}
